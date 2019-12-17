7cc9f4a6d06b517c8856da4ef91bf4f7bb5fb26e
Bradley Haley <bshaley25@gmail.com>
2019-12-17 19:48:42 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    result = {}
+    data.each do |key, value|
+      #binding.pry 
+        value.each do |key_1, value_1|
+          #binding.pry 
+          value_1.each do |name|
+
+             if result[name] == nil
+                result[name] = {}
+             end
+
+             new_attribute = key_1.to_s
+            
+             if result[name][key] == nil
+                new_attribute = key_1.to_s
+                result[name][key] = [new_attribute]
+             else
+                result[name][key] << new_attribute
+             end
+
+            end
+
+        end
+    end
+    result
 end
+
+
