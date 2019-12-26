f28d815f961c58edae4b9994d318f6606bccd82b
Evan Greer <etgreer91@gmail.com>
2019-12-26 04:29:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |characteristic, values|
+    values.each do |value, pigeons|
+      pigeons.each do |pigeon_name|
+        if pigeon_list[pigeon_name] == nil
+          pigeon_list[pigeon_name] = {}
+        end
+
+        if pigeon_list[pigeon_name][characteristic] == nil
+          pigeon_list[pigeon_name][characteristic] = []
+        end
+
+        pigeon_list[pigeon_name][characteristic] << value.to_s
+      end
+    end
+  end
+
+  pigeon_list
 end
